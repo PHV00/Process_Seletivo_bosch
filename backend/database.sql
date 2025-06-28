75d2665d5c9acb03f0931baad6f391ed4432767c
Pedro Vitoreti <133826934+PHV00@users.noreply.github.com>
2025-06-28 15:21:58 -0300

    feat: first commit, with strcuture of backend

---
+++ backend/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE reserve(
+    id int primary key identity(1,1),
+    resourceType nvarchar(8) not null,
+    responsibleName nvarchar(100) not null,
+    DateTimeStart datetime2 not null ,
+    hourDuration int not null check (hourDuration > 0 and hourDuration <= 8),
+    check (resourceType in ('room','vehicle','equipment')),
+    check (
+        cast(DateTimeStart as time) >= '08:00:00'
+        and
+        cast(DateTimeStart as time) <= '18:00:00'
+    ),
+);
